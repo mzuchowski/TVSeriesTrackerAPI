e17d848eb329eead4de8b6c856c011ebbeb3230c
Michał Zuchowski <michalzuch26@gmail.com>
2024-02-10 20:40:24 +0100

    Added DbSets, overide OnModelCreating method - for value objects

--- TVSeriesTracker/TVSeriesTracker.Persistance/TVSeriesDbContext.cs
+++ TVSeriesTracker/TVSeriesTracker.Persistance/TVSeriesDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TVSeriesTracker.Domain.Common;
+using TVSeriesTracker.Domain.Entities;
 
 namespace TVSeriesTracker.Persistance
 {
@@ -7,6 +8,32 @@ namespace TVSeriesTracker.Persistance
     {
         public TVSeriesDbContext(DbContextOptions<TVSeriesDbContext> options) : base(options)
         {
+        }
+
+        public DbSet<Director> Directors { get; set; }
+        public DbSet<Episode> Episodes { get; set; }
+        public DbSet<ExternalDataSourceDetail> ExternalDataSourceDetails { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MovieComment> MoviesComment { get; set; }
+        public DbSet<Season> Seasons { get; set; }
+        public DbSet<Series> Seriess { get; set; }
+        public DbSet<SeriesComment> SeriesComments { get; set; }
+        public DbSet<TrackedMovie> trackedMovies { get; set; }
+        public DbSet<TrackedSeries> TrackedSeries { get; set; }
+        public DbSet<Tracker> Trackers { get; set; }
+        public DbSet<TrackingStatus> TrackingStatuses { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Director>().OwnsOne(p => p.DirectorName);
+            modelBuilder.Entity<Writer>().OwnsOne(p => p.WriterName);
+            modelBuilder.Entity<SeriesComment>().OwnsOne(p => p.CommentContent);
+            modelBuilder.Entity<MovieComment>().OwnsOne(p => p.CommentContent);
+            modelBuilder.Entity<Series>().OwnsOne(p => p.SeriesProperies);
+            modelBuilder.Entity<Movie>().OwnsOne(p => p.MovieProperties);
         }
 
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
