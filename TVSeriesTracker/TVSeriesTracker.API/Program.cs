773ebbc2df7ac600f1c4e1eafba9c2be8e96ca1e
Michał Zuchowski <michalzuch26@gmail.com>
2024-11-20 16:46:58 +0100

    Added Automapper

--- TVSeriesTracker/TVSeriesTracker.API/Program.cs
+++ TVSeriesTracker/TVSeriesTracker.API/Program.cs
@@ -1,8 +1,10 @@
+using TVSeriesTracker.Application;
 using TVSeriesTracker.Persistance;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApplication();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddPersistance(builder.Configuration);
---
+++ TVSeriesTracker/TVSeriesTracker.Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TVSeriesTracker.Application.Common.Interfaces;
+
+namespace TVSeriesTracker.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
