6d934e8771bb70467532195932d81c17f1e18391
Michał Zuchowski <michalzuch26@gmail.com>
2024-11-21 17:05:03 +0100

    Added BaseCotroller and DirectorController

---
+++ TVSeriesTracker/TVSeriesTracker.API/Controllers/DirctorsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TVSeriesTracker.Application.Directors.Queries.GetDirectorDetail;
+
+namespace TVSeriesTracker.API.Controllers
+{
+    [Route("api/directors")]
+    public class DirctorsController : BaseController
+    {
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DirectorDetailVm>> GetDetails(int id)
+        {
+            var vm = await Mediator.Send(new GetDirectorDetailQuery { DirectorId = id });
+            return vm;
+        }
+    }
+}
